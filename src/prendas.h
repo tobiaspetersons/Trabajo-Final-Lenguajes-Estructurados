eaeabb9a4665cb6a4ac9f2424eafb9872eab4bc4
tobiaspetersons <tobiaspiter0206@gmail.com>
2025-06-03 17:43:44 -0300

    archivos

---
+++ src/prendas.h
@@ -0,0 +1,13 @@
+#ifndef PRENDAS_H
+#include <stdio.h>
+
+typedef struct
+{
+    int id;
+    char nombre[100];
+    char categoria[50];
+    float precio;
+}Prenda;
+
+
+#endif
---
+++ src/usuarios.h
@@ -0,0 +1,10 @@
+#ifndef USUARIOS_H
+#include <stdio.h>
+
+typedef struct
+{
+    char user[50];
+    char pass[50];
+}Usuario;
+
+#endif
