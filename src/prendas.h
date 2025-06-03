65824c97b9f9b7332bf9b29062f9fdfee7975000
erausquinsofi <erausquinsofia@gmail.com>
2025-06-03 18:07:45 -0300

    pruba

--- src/prendas.h
+++ src/prendas.h
@@ -3,10 +3,14 @@
 
 typedef struct
 {
-    int id;
+    int id; /*codigo imagen*/
     char nombre[100];
+    char color[100];
+    int talle;
     char categoria[50];
     float precio;
+    int stock;
+    int estado; /* 0: inactivo, 1:activo */
 }Prenda;
 
 
